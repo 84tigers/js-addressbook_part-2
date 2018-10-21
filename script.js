1cb4bca9665d72d69ebde11d99c4fd8de9e642d2
84tigers <ahyska@gmail.com>
2018-10-21 15:12:51 -0400

    js posting working, js console.log all info. need to call proper info

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 "use strict";
+
+
+
+
 class AddressBook {
     constructor() {
       this.contacts = [
@@ -13,10 +17,15 @@ class AddressBook {
     deleteAt(index) {
       this.contacts.splice(index, 1);
     }
-    print() {
-      for (let contact of this.contacts) {
-        console.log(contact);
-      }
+    get() {
+        // for (let contact of this.contacts) {
+        //   console.log(book[0].contact.name);
+          const container = document.querySelector(".id-card");
+          container.innerHTML = "";
+          const p = document.createElement("p");
+          p.innerText = `Name:  Email:  Phone:  Relation: `;
+          container.appendChild(p);
+        // }
     }
 };
 
@@ -29,21 +38,69 @@ class Contact {
     }
 };
 
+
 const book = new AddressBook();
-while (true) {
-    const choice = prompt("Would you like to Add, Remove, Print, or Quit?");
-    if (choice === "Add") {
-      const name = prompt("Enter name: ");
-      const phone = prompt("Enter phone: ");
-      const email = prompt("Enter email: ");
-      const relation = prompt("Enter relation: ");
-      book.add(new Contact(name, email, phone, relation));
-    } else if (choice === "Remove") {
-      const index = prompt("Enter an index to delete by.");
-      book.deleteAt(index);
-    } else if (choice === "Print") {
-      book.print();
-    } else if (choice === "Quit") {
-      break;
-    }
-};
+book.get();
+// const display = (contact) => {
+//     const container = document.querySelector(".id-card");
+//     container.innerHTML = "";
+//     const p = document.createElement("p");
+//     p.innerText = `Name: ${book.contact[0].name} Email: ${book.contact[0].email} Phone: ${book.contact[0].phone} Relation: ${book.contact[0].relation}`;
+//     container.appendChild(p);
+    
+//     // displayTotal(total);
+// };
+
+// get();
+
+
+//                 ************************** ORIGINAL ADRESS BOOK *****************************
+
+// class AddressBook {
+//     constructor() {
+//       this.contacts = [
+//         new Contact("bob", "bob@bob.com", "111", "weird neighbor"),
+//         new Contact("steve", "steve@steve.com", "111", "that uncle"),
+//         new Contact("sally", "sally@sally.com", "111", "the mom")
+//       ];
+//     }
+//     add(info) {
+//       this.contacts.push(info);
+//     }
+//     deleteAt(index) {
+//       this.contacts.splice(index, 1);
+//     }
+//     print() {
+//       for (let contact of this.contacts) {
+//         console.log(contact);
+//       }
+//     }
+// };
+
+// class Contact {
+//     constructor(name, email, phone, relation) {
+//       this.name = name;
+//       this.email = email;
+//       this.phone = phone;
+//       this.relation = relation;
+//     }
+// };
+
+// const book = new AddressBook();
+// while (true) {
+//     const choice = prompt("Would you like to Add, Remove, Print, or Quit?");
+//     if (choice === "Add") {
+//       const name = prompt("Enter name: ");
+//       const phone = prompt("Enter phone: ");
+//       const email = prompt("Enter email: ");
+//       const relation = prompt("Enter relation: ");
+//       book.add(new Contact(name, email, phone, relation));
+//     } else if (choice === "Remove") {
+//       const index = prompt("Enter an index to delete by.");
+//       book.deleteAt(index);
+//     } else if (choice === "Print") {
+//       book.print();
+//     } else if (choice === "Quit") {
+//       break;
+//     }
+// };
